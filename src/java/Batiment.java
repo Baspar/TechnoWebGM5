1e5b63ad8e96298182f5a9adf37adc208649417e
damien <damien.pointin@insa-rouen.fr>
2016-01-19 13:53:03 +0000

    debut mine

--- src/java/Batiment.java
+++ src/java/Batiment.java
@@ -1,8 +1,49 @@
 abstract class Batiment{
-    private String ressourceNecessaire;
-    private int pointsDeVie;
-    private int x;
-    private int y;
+    protected TypeRessource ressourceNecessaire;
+    protected int pointsDeVie;
+    protected int x;
+    protected int y;
+    protected int niveau;
+
+	public void setPointsDeVie(int in){
+		this.pointsDeVie = in;
+	}
+
+	public int getPointsDeVie(){
+		return this.pointsDeVie;
+	}
+	
+	public void setX(int in){
+		this.x = in;
+	}
+
+	public int getX(){
+		return this.x;
+	}
+
+	public void setY(int in){
+		this.y = in;
+	}
+	
+	public int getY(){
+		return this.y;
+	}
+
+	public void setNiveau(int in){
+		this.niveau = in;
+	}
+
+	public int getNiveau(){
+		return this.niveau;
+	}
+
+	public void setRessourceNecessaire(TypeRessource in){
+		this.ressourceNecessaire = in;
+	}
+
+	public TypeRessource getRessourceNecessaire(){
+		return this.ressourceNecessaire;
+	}
 
     abstract int calculerCoutUpgrade();
 }
--- src/java/MineOr.java
+++ src/java/MineOr.java
@@ -1,4 +1,20 @@
 public class MineOr extends Ressource{
+
+	public MineOr(){
+		pointsDeVie=360;
+		niveau=0;
+		x=-1;
+		y=-1;
+		ressourceNecessaire=TypeRessource.CHARBON;
+
+	}
+
+	public void upgrade(){
+		pointsDeVie+=40;
+		niveau+=1;
+
+	}
+
     public int calculerCoutUpgrade(){//TODO
         return 0;
     }
