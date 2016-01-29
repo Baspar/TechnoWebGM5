641f8f0fa7eef1840a0f55c2dbd620c5b7d89073
damien <damien.pointin@insa-rouen.fr>
2016-01-29 09:14:57 +0000

    upgrade defense

--- src/java/Defense.java
+++ src/java/Defense.java
@@ -1,20 +1,28 @@
 abstract class Defense extends Batiment{
     protected TypeAttaque typeDefense;
     protected int puissanceDefense;
+    protected float attaqueParSeconde; // On ne le prendra pas en compte dans les calculs juste au cas ou
     
-    public void setTypeDefense(TypeAttaque t){
+    public void setTypeDefense(TypeAttaque t){//DONE
         typeDefense=t;
     }
     
-    public TypeAttaque getTypeDefense(){
+    public TypeAttaque getTypeDefense(){//DONE
         return typeDefense;
     }
     
-    public void setPuissanceDefense(int puiss){
+    public void setPuissanceDefense(int puiss){//DONE
         puissanceDefense=puiss;
     }
     
-    public int getPuissanceDefense(){
+    public int getPuissanceDefense(){//DONE
         return puissanceDefense;
+    }
+    
+    
+    
+    public void upgrade(){//DONE
+        puissanceDefense+=puissanceDefense/5;
+        attaqueParSeconde+=0.1;
     }
 }
