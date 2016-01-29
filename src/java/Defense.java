52b7266b94ef0bb170f98605775aa12fd7200c52
damien <damien.pointin@insa-rouen.fr>
2016-01-29 09:09:49 +0000

    set et getter Defense

--- src/java/Defense.java
+++ src/java/Defense.java
@@ -1,4 +1,20 @@
 abstract class Defense extends Batiment{
     protected TypeAttaque typeDefense;
     protected int puissanceDefense;
+    
+    public void setTypeDefense(TypeAttaque t){
+        typeDefense=t;
+    }
+    
+    public TypeAttaque getTypeDefense(){
+        return typeDefense;
+    }
+    
+    public void setPuissanceDefense(int puiss){
+        puissanceDefense=puiss;
+    }
+    
+    public int getPuissanceDefense(){
+        return puissanceDefense;
+    }
 }
