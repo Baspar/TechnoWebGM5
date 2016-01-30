13e084868058001bf5afb92f59ead804901a12b2
damien <damien.pointin@insa-rouen.fr>
2016-01-30 22:29:06 +0000

    modif TypeRessource

--- src/java/Soldat.java
+++ src/java/Soldat.java
@@ -1,10 +1,12 @@
 abstract class Soldat{
-    protected String ressourceNecessaire;
+    protected TypeRessource ressourceNecessaire;
     protected int pointsDeVie;
     protected int puissanceAttaque;
     protected TypeAttaque typeAttaque;
     protected int coutUpgrade;
 
     abstract void update();
+    
+    
 
 }
