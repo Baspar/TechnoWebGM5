9bcc3abe995e844443933b0541d926498d0af744
damien <damien.pointin@insa-rouen.fr>
2016-01-30 22:54:26 +0000

    ajoutOubli + get/Set

--- src/java/Soldat.java
+++ src/java/Soldat.java
@@ -7,6 +7,43 @@ abstract class Soldat{
 
     abstract void update();
     
+    public TypeRessource getRessourceNecessaire(){//DONE
+        return ressourceNecessaire;
+    }
     
+    public void setRessourceNecessaire(TypeRessource t){//DONE
+        ressourceNecessaire=t;
+    }
 
+    public int getPointsDeVie(){//DONE
+        return pointsDeVie;
+    }
+    
+    public void setPointsDeVie(int i){//DONE
+        pointsDeVie=i;
+    }
+
+    public int getPuissanceAttaque(){//DONE
+        return puissanceAttaque;
+    }
+    
+    public void setPuissanceAttaque(int i){//DONE
+        puissanceAttaque=i;
+    }
+    
+    public void setTypeAttaque(TypeAttaque t){//DONE
+        typeAttaque=t;
+    }
+    
+    public TypeAttaque getTypeAttaque(){//DONE
+        return typeAttaque;
+    }
+    
+    public int getCoutUpgrade(){//DONE
+        return coutUpgrade;
+    }
+    
+    public void setCoutUpgrade(int c){//DONE
+        coutUpgrade=c;
+    }        
 }
