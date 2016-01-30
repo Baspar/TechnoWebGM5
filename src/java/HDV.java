f289baf5c4a74587f14d20139650c890fea44377
Blaine <bastien.laine@insa-rouen.fr>
2016-01-30 23:55:08 +0100

    [add] Ajout du quotaBatiment dans HDV

--- src/java/HDV.java
+++ src/java/HDV.java
@@ -1,7 +1,19 @@
+import java.util.Hashtable;
+
 public class HDV extends Batiment{
-    private Batiments batiment;
+    private Hashtable<TypeBatiment, Integer> quotaBatiments;
+
     public HDV(){//WIP
         typeBatiment=TypeBatiment.HDV;
+        quotaBatiments.put(TypeBatiment.HDV, 1);
+        quotaBatiments.put(TypeBatiment.Caserne, 1);
+        quotaBatiments.put(TypeBatiment.MineOr, 10);
+        quotaBatiments.put(TypeBatiment.MineCharbon, 10);
+        quotaBatiments.put(TypeBatiment.Mortier, 2);
+        quotaBatiments.put(TypeBatiment.Canon, 2);
+    }
+    public Integer getQuotaBatiment(TypeBatiment typeBatiment){//DONE
+        return quotaBatiments.get(typeBatiment);
     }
     public void upgrade(){//TODO
         super.upgrade();
