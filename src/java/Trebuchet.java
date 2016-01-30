cd25cbc34b3855e00c1a5f19c049cd1e4f7408da
damien <damien.pointin@insa-rouen.fr>
2016-01-30 23:35:16 +0000

    constructeurs divers soldats

--- src/java/Trebuchet.java
+++ src/java/Trebuchet.java
@@ -1,5 +1,15 @@
 public class Trebuchet extends Soldat{
-    Trebuchet(){//TODO
+    Trebuchet(){//DONE
+        ressourceNecessaire=TypeRessource.CHARBON;
+        pointsDeVie=300;
+        puissanceAttaque=11;
+        vitesseDeplacement=1;
+        typeAttaque=TypeAttaque.ZONE;
+        coutUpgrade=100000;
+        coutUnite=500;
+        niveau=1;
+        dureeFormation=120;
+        placeOccupee=5;
     }
    
    
