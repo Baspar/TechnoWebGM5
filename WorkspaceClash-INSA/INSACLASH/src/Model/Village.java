a5afa5e332963ae320412db45a69bc1e46fd6e86
damien <damien.pointin@insa-rouen.fr>
2016-01-31 22:00:06 +0000

    def methode village

--- WorkspaceClash-INSA/INSACLASH/src/Model/Village.java
+++ WorkspaceClash-INSA/INSACLASH/src/Model/Village.java
@@ -6,11 +6,57 @@ public class Village{
     private Armee armee;
     
     
-    Village(){//TODO
+    Village(){//DONE
+    	batiments=new Batiments();
+    	armee=new Armee();
     }
     
-    Village(String n){//TODO
-        Village t = new Village();
-        nom=n;
+    Village(String n){//DONE
+    	this();
+    	nom=n;
+    }
+    
+    public HDV getHDV(){//DONE
+    	return (HDV) batiments.getBatiments(TypeBatiment.HDV).get(0);
+    }
+    
+    public Caserne getCaserne(){//DONE
+    	return (Caserne) batiments.getBatiments(TypeBatiment.Caserne).get(0);
+    }
+
+	public String getNom(){//DONE
+		return nom;
+	}
+
+	public void setNom(String nom){//DONE
+		this.nom = nom;
+	}
+
+	public Batiments getBatiments(){//DONE
+		return batiments;
+	}
+
+	public void setBatiments(Batiments batiments){//DONE
+		this.batiments = batiments;
+	}
+
+	public Armee getArmee(){//DONE
+		return armee;
+	}
+
+	public void setArmee(Armee armee){//DONE
+		this.armee = armee;
+	}
+    
+    public boolean upgradeSoldat(TypeSoldat type){//TODO
+    	return false;
+    }
+    
+    public boolean upgradeBatiment(TypeBatiment type, int i){//TODO
+    	return false;
+    }
+    
+    public void preleverMine(TypeBatiment type, int i){//TODO
+    	
     }
 }
