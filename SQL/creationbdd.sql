30d6ca32f980c313577f3aea837170f62f990924
damien <damien.pointin@insa-rouen.fr>
2016-02-08 16:52:44 +0000

    table joueur

---
+++ SQL/creationbdd.sql
@@ -0,0 +1,20 @@
+create database plateforme;
+use plateforme;
+
+SET foreign_key_checks = 0;
+drop table Joueur;
+SET foreign_key_checks = 1;
+
+
+SELECT "Suppression tables termine" as "Message";
+
+
+create table Joueur(
+    login varchar(20) not null,
+    motDePasse varchar(56) not null,
+    
+    PRIMARY KEY(login)
+);
+
+
+    
