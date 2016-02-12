3351e87de9944a28ae29cc18547994277f5caaf5
Blaine <bastien.laine@insa-rouen.fr>
2016-02-12 16:58:57 +0100

    [fix] Retrait de tout les commentaire javadoc

--- WorkspaceClash-INSA/INSACLASH/src/Model/ConnexionForm.java
+++ WorkspaceClash-INSA/INSACLASH/src/Model/ConnexionForm.java
@@ -8,104 +8,40 @@ import org.jasypt.util.password.ConfigurablePasswordEncryptor;
 import dao.DAOException;
 import dao.JoueurDao;
 
-/**
- * Classe ConnexionForm representant un formulaire de connexion
- *
- * @author  Celine Chaugny & Damien Pointin
- */
 public final class ConnexionForm {
-    /**
-     * Le CHAMP_LOGIN correspond au login.
-     */
     private static final String CHAMP_LOGIN = "login";
 
 
-    /**
-     * Le CHAMP_PASS correspond au motDePasse.
-     */
     private static final String CHAMP_PASS = "motDePasse";
 
 
-    /**
-     * Le ALGO_CHIFFREMENT correspond à SHA-256.
-     */
     private static final String ALGO_CHIFFREMENT ="SHA-256";
 
 
-    /**
-     * Le resultat du formulaire.
-     */
     private String resultat;
 
 
-    /**
-     * Les erreurs du formulaire.
-     * Une map liant les mots clés d'erreur à la précision du type d'erreur
-     */
     private Map<String, String> erreurs = new HashMap<String, String>();
 
 
-    /**
-     * Le joueurDao du formulaire.
-     *
-     * @see JoueurDao
-     * @see InscriptionForm#InscriptionForm(JoueurDao)
-     */
     private JoueurDao joueurDao;
 
 
-    /**
-     * Retourne le resultat du formulaire.
-     *
-     * @return resultat du formulaire.
-     */
     public String getResultat() {
         return resultat;
     }
 
 
-    /**
-     * Retourne les erreurs du formulaire
-     *
-     * @return Map<String,String> correspondant aux erreurs.
-     */
     public Map<String, String> getErreurs() {
         return erreurs;
     }
 
 
-    /**
-     * Constructeur ConnexionForm.
-     * <p>
-     * Avec le parametre joueurDao
-     * </p>
-     *
-     * @param joueur
-     * Le joueurDao du formulaire.
-     *
-     * @see ConnexionForm#joueurDao
-     * @see JoueurDao
-     */
     public ConnexionForm( JoueurDao joueur){
         this.joueurDao=joueur;
     }
 
 
-    /**
-     * Connecte un joueur a la base de donnes si les donnees sont valides
-     *
-     * @param request
-     * la requete http
-     *
-     * @return le joueur qui est connecte
-     *
-     * @see ConnexionForm#getValeurChamp(HttpServletRequest, String)
-     * @see Joueur
-     * @see ConnexionForm#traiterLogin(String, Joueur)
-     * @see ConnexionForm#traiterMotDePasse(String, String, Joueur)
-     * @see JoueurDao
-     * @see DAOException
-     */
     public Joueur connecterJoueur( HttpServletRequest request ) {
         /* Récupération des champs du formulaire */
         String login = getValeurChamp( request, CHAMP_LOGIN );
@@ -131,19 +67,6 @@ public final class ConnexionForm {
     }
 
 
-    /**
-     * Vérifie la validite du login (deja present dans la table)
-     * si le login est valide on l'ajoute au joueur
-     *
-     * @param login
-     * le login du joueur a verifier
-     * @param joueur
-     * le joueur auquel on ajoute le login
-     *
-     * @see ConnexionForm#validationLogin(String)
-     * @see Joueur
-     * @see FormValidationException
-     */
     private void traiterLogin( String login, Joueur joueur ) {
         try {
             validationLogin( login );
@@ -154,16 +77,6 @@ public final class ConnexionForm {
     }
 
 
-    /**
-     * Validation du login du joueur
-     *
-     * @param login
-     * le login a verifie
-     *
-     * @throws FormValidationException Si le login n'est pas valide
-     *
-     * @see FormValidationException
-     */
     private void validationLogin( String login ) throws FormValidationException {
         if ( login != null && login.length() > 3 ) {
             if (joueurDao.trouver(login)==null){
@@ -175,21 +88,6 @@ public final class ConnexionForm {
     }
 
 
-    /**
-     * Validation du mot de passe
-     * par rapport au mot de passe crypte de la base
-     *
-     * @param login
-     * login du joueur dans la table
-     * @param motDePasse
-     * mot de passe a verifie
-     * @param joueur
-     * joueur auquel on ajoute le mot de passe
-     *
-     * @see ConnexionForm#validationMotDePasse(String, String)
-     * @see Joueur
-     * @see FormValidationException
-     */
     private void traiterMotDePasse( String login, String motDePasse, Joueur joueur) {
         try {
             validationMotDePasse(login, motDePasse );
@@ -199,19 +97,6 @@ public final class ConnexionForm {
     }
 
 
-    /**
-     * Validation du mot de passe du joueur
-     *
-     * @param motDePasse
-     * mot de passe du joueur
-     * @param lgin
-     * login du joueur
-     *
-     * @throws FormValidationException Si le mot de passe ne correspond pas
-     *
-     * @see ConfigurablePasswordEncryptor
-     * @see FormValidationException
-     */
     private void validationMotDePasse( String login, String motDePasse ) throws FormValidationException {
         if ( motDePasse != null ) {
             if ( motDePasse.length() > 3 ) {
@@ -229,28 +114,11 @@ public final class ConnexionForm {
     }
 
 
-    /**
-     * Ajoute un message correspondant au champ spécifié à la map des erreurs.
-     *
-     * @param champ
-     * champ spécifié pour l'erreur
-     * @param message
-     * message correspondant au type d'erreur
-     */
     private void setErreur( String champ, String message ) {
         erreurs.put( champ, message );
     }
 
 
-    /**
-     * Méthode qui retourne null si le champ est vide,
-     * son contenu sinon
-     *
-     * @param nomchamp
-     * champ spécifié
-     * @param request
-     * requete http
-     */
     private static String getValeurChamp( HttpServletRequest request, String nomChamp ) {
         String valeur = request.getParameter( nomChamp );
         if ( valeur == null || valeur.trim().length() == 0 ) {
