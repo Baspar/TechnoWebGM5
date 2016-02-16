f5dba6d629accbaccafc325300c9d6a20d6583da
Blaine <bastien.laine@insa-rouen.fr>
2016-02-16 20:29:14 +0100

    [add] Ajout tablea restante SQL

--- SQL/creationbdd.sql
+++ SQL/creationbdd.sql
@@ -10,11 +10,59 @@ SELECT "Suppression tables termine" as "Message";
 
 
 create table Joueur(
-    login varchar(20) not null,
-    motDePasse varchar(56) not null,
-    
+    login VARCHAR(20) not null,
+    motDePasse VARCHAR(56) not null,
+
     PRIMARY KEY(login)
 );
 
+create table Ressouce{
+    id INTEGER not null,
+    niveau INTEGER not null,
+    loginJoueur VARCHAR(20) not null,
+    typeRessouce ENUM('MineCharbon', 'MineOr')
+    derniereLevee TIMESTAMP not null,
+    x INTEGER not null,
+    y INTEGER not null,
 
-    
+    PRIMARY KEY(loginJoueur, typeRessource, id),
+    FOREIGN KEY(loginJoueur) REFERENCES Joueur(login)
+}
+create table Defense{
+    id INTEGER not null,
+    niveau INTEGER not null,
+    loginJoueur VARCHAR(20) not null,
+    typeDefense ENUM('Canon', 'Mortier')
+    x INTEGER not null,
+    y INTEGER not null,
+
+    PRIMARY KEY(loginJoueur, typeDefense, id),
+    FOREIGN KEY(loginJoueur) REFERENCES Joueur(login)
+}
+create table HDV{
+    niveau INTEGER not null,
+    loginJoueur VARCHAR(20) not null,
+    x INTEGER not null,
+    y INTEGER not null,
+
+    PRIMARY KEY(loginJoueur),
+    FOREIGN KEY(loginJoueur) REFERENCES Joueur(login)
+}
+create table Caserne{
+    niveau INTEGER not null,
+    loginJoueur VARCHAR(20) not null,
+    x INTEGER not null,
+    y INTEGER not null,
+
+    PRIMARY KEY(loginJoueur),
+    FOREIGN KEY(loginJoueur) REFERENCES Joueur(login)
+}
+create table Soldat{
+    id INTEGER not null,
+    niveau INTEGER not null,
+    loginJoueur VARCHAR(20) not null,
+    typeSoldat ENUM('Trebuchet', 'Archer')
+
+    PRIMARY KEY(loginJoueur, typeSoldat, id),
+    FOREIGN KEY(loginJoueur) REFERENCES Joueur(login)
+}
