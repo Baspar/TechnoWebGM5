ff422bad5053cd276b643e71704cd89c11909cc6
Blaine <bastien.laine@insa-rouen.fr>
2016-02-16 20:40:31 +0100

    [fix] Fix des probelem avec SQL. Tests OK

--- SQL/creationbdd.sql
+++ SQL/creationbdd.sql
@@ -1,68 +1,72 @@
-create database insaclash;
-use insaclash;
+CREATE DATABASE insaclash;
+USE insaclash;
 
 SET foreign_key_checks = 0;
-drop table Joueur;
+DROP TABLE Joueur;
+DROP TABLE Ressource;
+DROP TABLE Defense;
+DROP TABLE HDV;
+DROP TABLE Caserne;
+DROP TABLE Soldat;
 SET foreign_key_checks = 1;
 
 
-SELECT "Suppression tables termine" as "Message";
+SELECT "Suppression TABLEs termine" as "Message";
 
 
-create table Joueur(
-    login VARCHAR(20) not null,
-    motDePasse VARCHAR(56) not null,
+CREATE TABLE Joueur(
+    login VARCHAR(20) NOT NULL,
+    motDePasse VARCHAR(56) NOT NULL,
 
     PRIMARY KEY(login)
 );
-
-create table Ressouce{
-    id INTEGER not null,
-    niveau INTEGER not null,
-    loginJoueur VARCHAR(20) not null,
-    typeRessouce ENUM('MineCharbon', 'MineOr')
-    derniereLevee TIMESTAMP not null,
-    x INTEGER not null,
-    y INTEGER not null,
+CREATE TABLE Ressource(
+    id INTEGER NOT NULL,
+    niveau INTEGER NOT NULL,
+    loginJoueur VARCHAR(20) NOT NULL,
+    typeRessource ENUM('MineCharbon', 'MineOr'),
+    derniereLevee DATETIME NOT NULL,
+    x INTEGER,
+    y INTEGER,
 
     PRIMARY KEY(loginJoueur, typeRessource, id),
     FOREIGN KEY(loginJoueur) REFERENCES Joueur(login)
-}
-create table Defense{
-    id INTEGER not null,
-    niveau INTEGER not null,
-    loginJoueur VARCHAR(20) not null,
-    typeDefense ENUM('Canon', 'Mortier')
-    x INTEGER not null,
-    y INTEGER not null,
+);
+CREATE TABLE Defense(
+    id INTEGER NOT NULL,
+    niveau INTEGER NOT NULL,
+    loginJoueur VARCHAR(20) NOT NULL,
+    typeDefense ENUM('Canon', 'Mortier'),
+    x INTEGER NOT NULL,
+    y INTEGER NOT NULL,
 
     PRIMARY KEY(loginJoueur, typeDefense, id),
     FOREIGN KEY(loginJoueur) REFERENCES Joueur(login)
-}
-create table HDV{
-    niveau INTEGER not null,
-    loginJoueur VARCHAR(20) not null,
-    x INTEGER not null,
-    y INTEGER not null,
+);
+CREATE TABLE HDV(
+    niveau INTEGER NOT NULL,
+    loginJoueur VARCHAR(20) NOT NULL,
+    x INTEGER NOT NULL,
+    y INTEGER NOT NULL,
 
     PRIMARY KEY(loginJoueur),
     FOREIGN KEY(loginJoueur) REFERENCES Joueur(login)
-}
-create table Caserne{
-    niveau INTEGER not null,
-    loginJoueur VARCHAR(20) not null,
-    x INTEGER not null,
-    y INTEGER not null,
+);
+CREATE TABLE Caserne(
+    niveau INTEGER NOT NULL,
+    loginJoueur VARCHAR(20) NOT NULL,
+    x INTEGER NOT NULL,
+    y INTEGER NOT NULL,
 
     PRIMARY KEY(loginJoueur),
     FOREIGN KEY(loginJoueur) REFERENCES Joueur(login)
-}
-create table Soldat{
-    id INTEGER not null,
-    niveau INTEGER not null,
-    loginJoueur VARCHAR(20) not null,
-    typeSoldat ENUM('Trebuchet', 'Archer')
+);
+CREATE TABLE Soldat(
+    id INTEGER NOT NULL,
+    niveau INTEGER NOT NULL,
+    loginJoueur VARCHAR(20) NOT NULL,
+    typeSoldat ENUM('Trebuchet', 'Archer'),
 
     PRIMARY KEY(loginJoueur, typeSoldat, id),
     FOREIGN KEY(loginJoueur) REFERENCES Joueur(login)
-}
+);
